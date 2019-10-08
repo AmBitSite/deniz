e259acd1d187f74d8520387ab4d2846d9eaa821d
EvaMaster <immortalkill123@gmail.com>
2019-10-08 10:08:50 +0300

    ss

--- gulpfile.js
+++ gulpfile.js
@@ -2,64 +2,68 @@
 var gulp = require('gulp'),
     cssmin = require('gulp-minify-css'),
     fileinclude = require('gulp-file-include'),
-    watch = require("gulp-watch"),
-    livereload = require('gulp-livereload');
-    var browserSync  = require('browser-sync').create();
- 
-gulp.task('html:build',(done)=>{
+    browserSync = require('browser-sync').create();
+
+gulp.task('html:build', (done) => {
     gulp.src('src/*.html')
-        .pipe(fileinclude())
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: 'src/template/'
+          }))
         .pipe(gulp.dest('build/'))
-        // .pipe(livereload({ start: true }))
-        done();
+    done();
 });
-gulp.task('css:build', (done)=> {
+gulp.task('css:build', (done) => {
     gulp.src("src/css/*.css")
         .pipe(cssmin())
         .pipe(gulp.dest("build/css"))
         .pipe(browserSync.stream());
 
-done();
+    done();
 });
-gulp.task('img:build', (done)=> {
+gulp.task('img:build', (done) => {
     gulp.src("src/img/*.*")
         .pipe(gulp.dest("build/img"))
 
-done();
+    done();
 });
-gulp.task('font:build', (done)=> {
+gulp.task('font:build', (done) => {
     gulp.src("src/font/*.*")
         .pipe(gulp.dest("build/font"))
 
-done();
+    done();
 });
-gulp.task('js:build', (done)=> {
+gulp.task('js:build', (done) => {
     gulp.src("src/js/*.*")
         .pipe(gulp.dest("build/js"))
 
-done();
+    done();
 });
-gulp.task(
-    'default', 
-        gulp.series('html:build', 'css:build', 'img:build', 'font:build', 'js:build')
-);
 
-
-
-
-
-gulp.task('serve', (done)=> {
+gulp.task('build', (done) => {
+        gulp.series(['css:build','html:build','img:build','font:build','js:build'])
+    done();
+});
+gulp.task('serve', (done) => {
     browserSync.init({
-    server: "src/"
+        server: "build/"
     });
-    gulp.watch("src/css/*.css").on('change', browserSync.reload);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-    
+    gulp.watch("src/css/*.css").on('change', gulp.series('css:build'));
+    gulp.watch("src/*.html").on('change', gulp.series('html:build'));
+    gulp.watch("src/img/*.*").on('change', gulp.series('img:build'));
+    gulp.watch("src/font/*.*").on('change', gulp.series('font:build'));
+    gulp.watch("src/js/*.js").on('change', gulp.series('js:build'));
     done()
 });
-
-gulp.task('watch', (done)=> {
-    watch("glob", ['change'], ['html:build', 'css:build', 'img:build', 'font:build', 'js:build'])
+gulp.task("watch",(done)=>{
+    browserSync.init({
+        server: "build/"
+    });
+    gulp.watch("src/css/*.css").on('change', gulp.series(['css:build',browserSync.reload]));
+    gulp.watch("src/*.html").on('change', gulp.series(['html:build',browserSync.reload]));
+    gulp.watch("src/img/*.*").on('change', gulp.series(['img:build',browserSync.reload]));
+    gulp.watch("src/font/*.*").on('change', gulp.series(['font:build',browserSync.reload]));
+    gulp.watch("src/js/*.js").on('change', gulp.series(['js:build',browserSync.reload]));
     done()
-});
+})
 
