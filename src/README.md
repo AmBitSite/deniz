ac2c6bdfd9197ae6f21e3a4299e09ef57002032f
EvaMaster <immortalkill123@gmail.com>
2019-10-08 10:41:51 +0300

    08.10.19

---
+++ src/README.md
@@ -0,0 +1,1 @@
+# https://ambitsite.github.io/deniz/.
---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+var test = document.querySelector(".input__mail");
+var test1 = document.querySelector(".form-send__dynamic-text");
+if (test) {
+    test.addEventListener("keyup", function () {
+        test1.innerText = test.value;
+    })
+}
+
+var arrMoney =['CNY','CHF','GBP','USD', 'TRY'];
+var moneyName = document.querySelectorAll(".info-converter-row__name");
+var moneyValue = document.querySelectorAll(".info-converter-row__value");
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            // console.log(this.responseText)
+            for(var i = 0; i<moneyName.length; i++){
+                moneyName[i].innerText = arrMoney[i]
+                moneyValue[i].innerText=JSON.parse(this.responseText).rates[arrMoney[i]]
+            }
+        }
+    });
+    xhr.open("GET", "https://api.exchangeratesapi.io/latest?base=EUR", true);
+    xhr.send()
+
