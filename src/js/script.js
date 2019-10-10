b46af0d01acb4d6982ca2aa9233078db67e08755
EvaMaster <immortalkill123@gmail.com>
2019-10-10 11:35:19 +0300

    10.10.19

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,23 @@
 var test = document.querySelector(".input__mail");
 var test1 = document.querySelector(".form-send__dynamic-text");
+var arrMoney = ['CNY', 'CHF', 'GBP', 'USD', 'TRY'];
+var moneyName = document.querySelectorAll(".info-converter-row__name");
+var moneyValue = document.querySelectorAll(".info-converter-row__value");
+var blockNews = document.getElementsByClassName("info-news-block");
+var arrCryptoMoney = ['BTC', 'ETH', 'BCH', 'USDT', 'LTC'];
+var moneyCryptoName = document.querySelectorAll(".info-converter-crypto-row__name");
+var moneyCryptoValue = document.querySelectorAll(".info-converter-crypto-row__value");
+var sliderParent = document.querySelector(".slider-wrap")
+var sliderControls = document.querySelector(".slider-control")
+var slideItem1 = document.querySelector(".slider-wrap__1")
+var slideItem2 = document.querySelector(".slider-wrap__2")
+var slideItem3 = document.querySelector(".slider-wrap__3")
+
 if (test) {
     test.addEventListener("keyup", function () {
         test1.innerText = test.value;
     })
 }
-
-var arrMoney = ['CNY', 'CHF', 'GBP', 'USD', 'TRY'];
-var moneyName = document.querySelectorAll(".info-converter-row__name");
-var moneyValue = document.querySelectorAll(".info-converter-row__value");
 
 var xhr = new XMLHttpRequest();
 xhr.withCredentials = false;
@@ -23,9 +32,6 @@ xhr.addEventListener("readystatechange", function () {
 xhr.open("GET", "https://api.exchangeratesapi.io/latest?base=EUR", true);
 xhr.send()
 
-
-var blockNews = document.getElementsByClassName("info-news-block");
-
 var xhrN = new XMLHttpRequest();
 xhrN.withCredentials = false;
 xhrN.addEventListener("readystatechange", function () {
@@ -37,9 +43,6 @@ xhrN.addEventListener("readystatechange", function () {
 xhrN.open("GET", "https://newsapi.org/v2/top-headlines?category = business&sources=bloomberg&apiKey=d5ab78edfa2649a6b0fd66a7cf1c2c68", true);
 xhrN.send()
 
-var arrCryptoMoney = ['BTC', 'ETH', 'BCH', 'USDT', 'LTC'];
-var moneyCryptoName = document.querySelectorAll(".info-converter-crypto-row__name");
-var moneyCryptoValue = document.querySelectorAll(".info-converter-crypto-row__value");
 var xhrC = new XMLHttpRequest();
 xhrC.withCredentials = false;
 xhrC.addEventListener("readystatechange", function () {
@@ -53,18 +56,10 @@ xhrC.addEventListener("readystatechange", function () {
 xhrC.open("GET", "https://min-api.cryptocompare.com/data/price?fsym=EUR&tsyms=BTC,ETH,BCH,USDT,LTC&apiKey=bb98291570d521612ebd320b47a541e57dd03581bc116ddeb19abb62e7a306a6", true);
 xhrC.send()
 
-
-
-var sliderParent = document.querySelector(".slider-wrap")
-var sliderControls = document.querySelector(".slider-control")
-var slideItem1 = document.querySelector(".slider-wrap__1")
-var slideItem2 = document.querySelector(".slider-wrap__2")
-var slideItem3 = document.querySelector(".slider-wrap__3")
-
 sliderControls.addEventListener("click", (e)=>{
     var btnSlider = e.target;
     var activeClass = document.querySelector(".slider-control__item_active")
-    if(activeClass){
+    if(activeClass && btnSlider.classList.contains("slider-control__item")){
         activeClass.classList.remove("slider-control__item_active")
         btnSlider.classList.add("slider-control__item_active")
         var activeClass = document.querySelector(".slider-control__item_active")
