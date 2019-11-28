575655fb0b602ca0d449f64085cccabd8a9097f7
AmBitSite <yfljvtymitgbnmrtif@yandex.ru>
2019-11-28 17:53:10 +0300

    28.11.2019

--- build/js/script.js
+++ build/js/script.js
@@ -42,18 +42,20 @@ xhrN.addEventListener("readystatechange", function () {
 });
 
 // ---------------------------------------httprequest cryptocurrency courses --------------------------------------------------
-var xhrC = new XMLHttpRequest();
-xhrC.withCredentials = false;
-xhrC.open("GET", "https://min-api.cryptocompare.com/data/price?fsym=EUR&tsyms=BTC,ETH,BCH,USDT,LTC&apiKey=bb98291570d521612ebd320b47a541e57dd03581bc116ddeb19abb62e7a306a6", true);
-xhrC.send();
-xhrC.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-        for (var i = 0; i < arrCryptoMoney.length; i++) {
-            moneyCryptoName[i].innerText = arrCryptoMoney[i];
-            moneyCryptoValue[i].innerText = JSON.parse(this.responseText)[arrCryptoMoney[i]];
+if (moneyCryptoName == undefined) {
+    var xhrC = new XMLHttpRequest();
+    xhrC.withCredentials = false;
+    xhrC.open("GET", "https://min-api.cryptocompare.com/data/price?fsym=EUR&tsyms=BTC,ETH,BCH,USDT,LTC&apiKey=bb98291570d521612ebd320b47a541e57dd03581bc116ddeb19abb62e7a306a6", true);
+    xhrC.send();
+    xhrC.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            for (var i = 0; i < arrCryptoMoney.length; i++) {
+                moneyCryptoName[i].innerText = arrCryptoMoney[i];
+                moneyCryptoValue[i].innerText = JSON.parse(this.responseText)[arrCryptoMoney[i]];
+            };
         };
-    };
-});
+    });
+}
 
 // -----------------------------------------------   Slider   ----------------------------------------------------------------
 function sliderCart(arrElem, count) {
@@ -83,12 +85,12 @@ function runSlider() {
     for (let i = 0; i < arrControlSlider.length; i++) {
         if (arrControlSlider[i].classList.contains("slider-control__item_active")) {
             arrControlSlider[i].classList.remove("slider-control__item_active")
-            if (i!= arrControlSlider.length-1) {
+            if (i != arrControlSlider.length - 1) {
                 arrControlSlider[`${i += 1}`].classList.add("slider-control__item_active")
                 sliderCart(sliderParent, i)
             }
-            else{
-                i=0;
+            else {
+                i = 0;
                 arrControlSlider[i].classList.add("slider-control__item_active")
                 sliderCart(sliderParent, i)
             }
@@ -96,7 +98,7 @@ function runSlider() {
     }
 }
 
-setInterval(runSlider ,5000)
+setInterval(runSlider, 5000)
 // -----------------------------------------------authorization----------------------------------------------------------
 var authorizationBlock = document.querySelector(".internet-banking");
 var btnBlock = document.querySelector(".authorization-block-btn");
@@ -109,8 +111,20 @@ function hideCildrenElements(element) {
 }
 
 
-hideCildrenElements(authorizationBlock);
-authorizationBlock.children[0].style.display = "block";
+if (authorizationBlock) {
+    hideCildrenElements(authorizationBlock);
+    authorizationBlock.children[0].style.display = "block";
+    authorizationBlock.addEventListener("click", function (e) {
+        if (e.target === document.querySelector(".internet-banking__btn")) {
+
+            hideCildrenElements(authorizationBlock);
+            showElement(authorizationBlock.children[1]);
+            showElement(btnBlock);
+            showElement(btnBlock.children[0]);
+            showElement(btnBlock.children[1]);
+        }
+    })
+}
 
 function showElement(elem) {
     elem.style.display = "block";
@@ -121,39 +135,80 @@ function hideElement(elem) {
 
 
 
-
-authorizationBlock.addEventListener("click", function (e) {
-
-    if (e.target === document.querySelector(".internet-banking__btn")) {
-        hideCildrenElements(authorizationBlock);
-        showElement(authorizationBlock.children[1]);
-        showElement(btnBlock);
-    }
-})
-
-btnBlock.children[1].addEventListener("click", function () {
-    if (!pointAuthorithation) {
-        hideCildrenElements(authorizationBlock);
-        showElement(authorizationBlock.children[2]);
-        if (pointAuthorithation < 1) {
-            pointAuthorithation = 1;
+if (btnBlock) {
+    btnBlock.children[1].addEventListener("click", function () {
+        var obj = {
+            username: document.querySelector(".authorization__login").value,
+            password: document.querySelector(".authorization__password").value
         }
-    }
-    else if (pointAuthorithation) {
-        hideCildrenElements(authorizationBlock);
-        showElement(authorizationBlock.children[3]);
-        hideElement(btnBlock.children[1]);
-        if (pointAuthorithation) pointAuthorithation = 0;
-    }
-    else {
-        hideCildrenElements(authorizationBlock);
-        showElement(authorizationBlock.children[3]);
+        var objS = JSON.stringify(obj)
+        console.log(objS);
 
-        pointAuthorithation = 0;
-    }
-});
-btnBlock.children[0].addEventListener("click", function () {
-    hideCildrenElements(authorizationBlock);
-    authorizationBlock.children[0].style.display = "block";
-    hideElement(btnBlock.children[0]);
-})
+        let promise = new Promise((resolve, reject) => {
+            var xhrd = new XMLHttpRequest();
+            xhrd.withCredentials = false;
+            xhrd.open("POST", "http://watch.samtsov.com:8070/api/login_check", true);
+            xhrd.send(objS);
+            xhrd.addEventListener("readystatechange", function () {
+                console.log(this.readyState)
+                console.log(this.DONE)
+                if (this.status == 200) {
+                    resolve(sessionStorage.setItem("token", JSON.parse(this.responseText).token))
+                }
+                else {
+                    var error = new Error(this.statusText);
+                    error.code = this.status;
+                    reject(error);
+                }
+            });
+        })
+        promise
+            .then(
+                result => {
+                    // hideCildrenElements(authorizationBlock);
+                    // showElement(authorizationBlock.children[2]);
+                    window.location.href = "http://localhost:3000/account.html"
+                },
+                error => {
+                    hideCildrenElements(authorizationBlock);
+                    showElement(authorizationBlock.children[3]);
+                    hideElement(btnBlock.children[1]);
+                }
+            );
+    });
+    btnBlock.children[0].addEventListener("click", function () {
+        hideCildrenElements(authorizationBlock);
+        authorizationBlock.children[0].style.display = "block";
+        hideElement(btnBlock.children[0]);
+        hideElement(btnBlock.children[1]);
+    })
+}
+
+// $(window).on("load", function () {
+//     fetch("http://watch.samtsov.com:8070/public/product/gets/products", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify({
+//         "brand": INPUT_BRAND
+//       })
+//     })
+//     .then(response => response.json())
+//     .then(result => startLogic(result));
+//   });
+var arrTabs = document.getElementsByClassName("menu-wrap-item__input");
+var blockArrTabs = document.querySelector(".menu-wrap-contain");
+var blockArrMenu = document.querySelector(".menu-bord");
+
+if (blockArrTabs) {
+    blockArrTabs.addEventListener("click", function () {
+        for (let i = 0; i <= arrTabs.length - 1; i++) {
+            if (arrTabs[i].checked) {
+                document.querySelector(".d-flex").classList.remove("d-flex")
+                blockArrMenu.children[i].classList.add("d-flex")
+                // console.log(i)
+            }
+        }
+    })
+}
